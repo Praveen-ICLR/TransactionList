b78145acdc5585be0132761051f89542260deaa0
Praveen Rajendran <praveen@inteliclear.com>
2022-06-07 11:17:33 -0400

    first commit

---
+++ TL/CommandClass.cs
@@ -0,0 +1,30 @@
+﻿using inteliclear;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+
+namespace TL
+{
+    public class CommandClass
+    {
+        private int AcctLoadCommandTimeout = 12000;
+        public DbCommand GetTranslist()
+        {
+            DbProviderFactory ICprovider = icGeneric.GetProvider();
+            DbCommand ICcmd = ICprovider.CreateCommand();
+            ICcmd.CommandText = "translist";
+            ICcmd.CommandType = CommandType.StoredProcedure;
+            ICcmd.CommandTimeout = AcctLoadCommandTimeout;
+
+            return ICcmd;
+        }
+        
+        
+       
+
+    }
+}
---
+++ TL/Program.cs
@@ -0,0 +1,36 @@
+﻿using inteliclear;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+
+namespace TL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            DataTable dt = new DataTable();
+            DbCommand ICcmd;
+            CommandClass objCC = new CommandClass();
+            DataAccess objDA = new DataAccess();
+            ICcmd = objCC.GetTranslist();
+            dt = objDA.GetData(ICcmd);
+
+            if(dt.Rows.Count > 0)
+            {
+                Console.WriteLine(dt);
+            }
+            else
+            {
+                Console.WriteLine("Failed Process..");
+            }
+
+        }
+    }
+}
